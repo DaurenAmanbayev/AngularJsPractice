6f96262bd16d35b05cd90e01c922661cb81d5213
Dauren Amanbayev <daur_aman@mail.ru>
2016-07-12 17:01:46 +0600

    Прототип для второй задачи

---
+++ 002_TemplateDerectives/Samples/Task/quizApp.js
@@ -0,0 +1,29 @@
+﻿var model=[{
+            value: '1',                
+            question: "Question 1",
+            answers: [
+                { content: "HTML, CSS", check: true, pass:false },
+                { content: "JavaScript Essential", check: false, pass:false },
+                { content: "JavaScript Advanced", check: false, pass: false },
+                { content: "HTML5 &amp; CSS3", check: false pass: false },
+                { content: "AngularJS", check: false, pass: false }]
+            },
+            {
+            value: '2',
+            question: "Question 2",
+            answers: [
+                { content: "Marvel", check: true },
+                { content: "DC", check: false },
+                { content: "Shonen Jump", check: false }]                
+            }
+        ];
+
+var app = angular.module("testApp", []);
+
+app.controller("TestAppCtrl", function ($scope) {
+
+    $scope.quiz = model;    
+  
+    $scope.mode = $scope.quiz[0];
+
+});
